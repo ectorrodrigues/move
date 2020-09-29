9dd47f359e48d0ff7aa3605102b9d61d76414886
Éctor Rodrigues <ectorrodrigues@gmail.com>
2020-09-29 01:56:49 -0300

    logo index url

--- app/view/elements/site/top.php
+++ app/view/elements/site/top.php
@@ -2,7 +2,7 @@
   <div class="row justify-content-center py-2">
     <div class="col-lg-10 px-lg-0 px-5 mt-lg-0 mt-4">
       <div class="row justify-content-between px-lg-0 px-3">
-        <a href="/<?=SITE_NAME?>" class="d-inline-block text-left">
+        <a href="/" class="d-inline-block text-left">
           <img src="<?=FILES_DIR?>logo.svg" alt="logo" class="col-lg-5 col-10">
         </a>
         <nav class="d-inline-block text-right">
